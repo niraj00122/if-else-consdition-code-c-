bf80b3acc28de4deabf2d548d8b61c2e49cb8a8c
niraj00122 <ny074883@gmail.com>
2024-06-27 14:57:17 +0530

    insialize

---
+++ New folder/main.cpp
@@ -0,0 +1,81 @@
+// Your First C++ Program
+
+#include<iostream>
+using namespace std;
+int main(){
+    // int a;
+    // cin>>a;
+    // if(a>0){
+    //     cout<<"A is posative:";
+        
+    // }
+    // else{
+    //     cout<<"A is negative";
+    // }
+    
+    
+    // int a,b;
+    // cout<<"Enter the value of A";
+    // cin>>a;
+    // cout<<"Enter the value of B";
+    // cin>>b;
+    // if(a>b){
+    //     cout<<"A is gretaer";
+    // }
+    // if(b>a){
+    //     cout<<"B is greater";
+    // }
+    
+    
+//   int a=24;
+//   if(a>20){
+//       cout<<"Love";
+//   }
+//   else if(a==24){
+//       cout<<"Lovely";
+//   }
+//   else{
+//       cout<<"babbr";
+//   }
+//   cout<<a;
+
+
+// int n;
+// cout<<"Enter the number";
+// cin>>n;
+// int i=1;
+
+// while(i<=n){
+//     cout<<i<<" ";
+//     i=i+1;
+// }
+ 
+ //sum number print 
+//  int n;
+//  cin>>n;
+//  int i=1;
+//  int sum=0;
+//  while(i<=n){
+//      sum=sum+i;
+//      i=i+1;
+//  }
+//  cout<<"Value of sum is "<<sum;
+ 
+//prime number 
+int n;
+cin>>n;
+ int i=2;
+ while(i<n){
+     //divising 
+     if(n%i==0){
+         cout<<"Not prime";
+     }
+     else
+     {
+         cout<<"Prime";
+     }
+     i=i+1;
+ }
+     
+     
+ }
